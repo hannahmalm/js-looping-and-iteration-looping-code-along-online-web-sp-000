f3b3e648a7b9a1ef17549c9d2b0dabc40e2a170e
Hannah M <hannahemalm616@gmail.com>
2021-04-08 16:33:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+
+const names =["Ada", "Brendan", "Ali"];
+function writeCards(names,event) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you,${names[i]}, for the wonderful surprise gift!`);
+  }
+ 
+  return names;
+}
